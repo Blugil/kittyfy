b2f7d1dffba5fae049cc6cb285d48ce18e14eef9
Dominick Agnello <dagnello@asu.edu>
2021-07-15 17:13:10 -0700

    (day 5) already questioning validation, will rethink

--- parser.py
+++ parser.py
@@ -39,13 +39,15 @@ def parseKitty():
     else:
         return
 
-
+#TODO: make sure validation works even if out of order
+# rethink this functions purpose, do we want to enable themes to set only
+# the values within the theme or be required to set all values
 def validateTheme(filename):
     
     # this is lazy programming, other file already has a load fn
     # just for test do NOT use in prod, no need to open files twice
     data = json.load(open(filename, 'r'))
-    keys = list(data.keys())
+    theme_keys = list(data.keys())
 
     # temp proof of concept
     colors = ['hello', 'test'] 
@@ -53,8 +55,8 @@ def validateTheme(filename):
     ## checks to make sure all required colors are in the theme.json file
     for i, color in enumerate(colors):
         try:
-            print(keys[i], color)
-            if keys[i] != color:
+            print(theme_keys[i], color)
+            if theme_keys[i] != color:
                 return False
         except:
             print('Exception handled')
