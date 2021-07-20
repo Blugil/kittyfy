ada6e8f832f5f471ec181742dab902a56e5e6d52
Dominick Agnello <dagnello@asu.edu>
2021-07-19 20:39:01 -0700

    (day 9) 'finished' file i/o

--- util.py
+++ util.py
@@ -1,3 +1,6 @@
+import json
+
+
 kitty_theme_options = ['color0','color1','color2','color3','color4','color5','color6','color7','color8', 'color9','color10','color11','color12','color13','color14','color15','background', 'foreground', 'selection_background', 'selection_foreground']
 
 # proof of concept parser that looks through a config file and prints (for now) each matching
@@ -111,22 +114,37 @@ def readKitty(filename: str):
             cond = False
 
         current_line += 1
-
+    
+    data.close()
     return document
 
-# read the entire file into an array (like current readKitty but without avoiding reading some lines, then remove (or replace) the strings at certain indexes given as line numbers by the parseKitty(function), then just store the file
-
-def replace(new_kitty: list, new_theme: dict):
+# adds new theme values in a constructed string to the array
+def update(new_kitty: list, new_theme: dict):
     
-    # parse through the kitty config file first
-    # open kitty config to append
+    new_kitty = new_kitty
+
+    keys = new_theme.keys()
+
+    for key in keys: 
+        string = key + ' ' + new_theme[key] + '\n'
+        new_kitty.append(string)
+
     
+    return new_kitty
+    
+def replace(filename: str, data: list):
 
+    kitty = open(filename, 'wt')
+    kitty.writelines(data)
+    kitty.close()
 
-    return 1
 
 if __name__ == "__main__":
 
     # print(parseKitty('./theme.conf'))
-    print(readKitty('./theme.conf'))
+    # print(readKitty('./theme.conf'))
+    new_theme = json.load(open('./themes/gruvbox.json'))
+    kitty = update(readKitty('./theme.conf'), new_theme)
+
+    replace('./theme.conf', kitty)
 
